ca6be42e59683c750c7fc368ba7b4c2bcb3db90a
Simon Knott <info@simonknott.de>
2021-02-16 18:33:52 +0100

    Ignore %s

--- src/encodeRedisKey.ts
+++ src/encodeRedisKey.ts
@@ -1,7 +1,7 @@
 export function encodeRedisKey(decoded: string): string {
-  return decoded.replace(/%/g, "%25").replace(/:/g, "%3A");
+  return decoded.replace(/:/g, "%3A");
 }
 
 export function decodeRedisKey(encoded: string): string {
-  return encoded.replace(/%3A/g, ":").replace(/%25/g, "%");
+  return encoded.replace(/%3A/g, ":");
 }
